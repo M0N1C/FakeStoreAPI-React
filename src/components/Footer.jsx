700023da77e206d3c1313ec20f3b43ef8d0d2236
monica am <monica.andres.consulting@gmail.com>
2024-11-15 17:37:00 +0100

    FAKEAPI

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css'; 
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; FakeStore - CRUD</p>
+      <p>API Documentacion <a href="https://fakestoreapi.com/docs" target="_blank" rel="noopener noreferrer">Fake Store API</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
